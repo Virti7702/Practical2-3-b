6d64ad01ae35afd85eb8239d29fbdd06e0c3b931
Virti7702 <97676077+Virti7702@users.noreply.github.com>
2022-01-14 15:13:33 +0530

    Add files via upload

---
+++ pratical_2_3_b.py
@@ -0,0 +1,10 @@
+'''
+Name - Virti Shah
+ID - 20CE135
+Aim - A python program to remove an item from the set.
+Github link - https://github.com/Virti7702/Practical2-3-b.git
+
+'''
+t={1,2,3}
+t.remove(2)
+print(t)
